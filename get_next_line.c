2c802453cccb1bd677bb691e821d2574cd9d485f
Safiy Errahmane ZAGHBANE <szaghbane@gmail.com>
2018-08-11 17:26:13 +0200

    [FEATURE] Read from muilti files
    - Remove libft static dir
    - Add 42FileChecker tests
    - Update Travis-ci script

--- get_next_line.c
+++ get_next_line.c
@@ -14,28 +14,50 @@
 #include <stdio.h>
 
 
-t_gnl	*ft_fetch_or_create(t_gnl	*list, int	fd)
+static t_gnl	*ft_fetch_or_create(t_gnl **list, const int fd)
 {
-	t_gnl	*node = list;
+	t_gnl	*node;
 
-	if (node == NULL) {
-		SECURE((list = (t_gnl*)malloc(sizeof(t_gnl))));
-		list->fd = fd;
-		list->bindex = 0;
-		return (list);
-	}
-	while (node != NULL && node->fd != fd) {
-		if (node->next == NULL) {
-			SECURE((node->next = (t_gnl*)malloc(sizeof(t_gnl))));
-			node->next->fd = fd;
-			list->bindex = 0;
-			return (node->next);
-		}
+	node = *list;
+	while (node)
+	{
+		if (node->fd == fd)
+			return (node);
 		node = node->next;
 	}
-	if (node != NULL && node->fd == fd)
-		return node;
-	return (NULL);
+	SECURE((node = (t_gnl *)malloc(sizeof(t_gnl))));
+	node->fd = fd;
+	node->buffer = NULL;
+	node->next = *list;
+	*list = node;
+	return (node);
+}
+
+
+static char		*find_and_store(t_gnl *curr, char buffer[BUFF_SIZE + 1])
+{
+	char	*tmp;
+	char	*sub;
+	char	*nlc;
+	char	*str;
+	size_t	size;
+
+	str = ft_strnew(0);
+	while (!(nlc = ft_strchr(buffer, '\n')))
+	{
+		SWAP_FREE(tmp, str, ft_strjoin(str, buffer));
+		if (!(size = read(curr->fd, buffer, BUFF_SIZE)))
+		{
+			ft_strdel(&(curr->buffer));
+			return (str);
+		}
+		buffer[size] = '\0';
+	}
+	sub = ft_strsub(buffer, 0, NL_INDEX);
+	SWAP_FREE(tmp, str, ft_strjoin(str, sub));
+	free(sub);
+	SWAP_FREE(tmp, curr->buffer, ft_strsub(buffer, NL_INDEX + 1, BUFF_SIZE - NL_INDEX));
+	return (str);
 }
 
 
@@ -43,22 +65,22 @@ int	get_next_line(const int	fd, char	**line)
 {
 	static t_gnl	*list = NULL;
 	t_gnl			*curr;
-	int				ret;
+	char			buffer[BUFF_SIZE + 1];
+	size_t			size;
 
-	HANDEL_ERROR(fd, read(fd, NULL, 0), line);
-	CHECK((curr = ft_fetch_or_create(list, fd)));
-	while ((ret = read(fd, curr->buffer, BUFF_SIZE)) >= 0) {
-		SECURE((*line = (char*)malloc(BUFF_SIZE * sizeof(char))));
-		ft_strcpy(*line, curr->buffer);
-		curr->bindex += ret;
-		ft_strchr(curr->buffer, '\n');
-		
-		printf("%d\n", curr->bindex);
-		printf("%s\n", curr->buffer);
-		printf("%s\n", *line);
-		return (EXIT_END);
+	HANDEL_ERROR(fd, read(fd, NULL, 0), line, BUFF_SIZE);
+	curr = ft_fetch_or_create(&list, fd);
+	if (curr->buffer && ft_strlen(curr->buffer))
+		ft_strcpy(buffer, curr->buffer);
+	else
+	{
+		if (!(size = read(fd, buffer, BUFF_SIZE)))
+		{
+			ft_strdel(&(curr->buffer));
+			return (EXIT_END);
+		}
+		buffer[size] = '\0';
 	}
-	IS_END(ret);
-	IS_ERROR(ret);
+	*line = find_and_store(curr, buffer);
 	return (EXIT_SUCCUESS);
 }
